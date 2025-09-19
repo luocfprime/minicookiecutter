5274bfe931d3831a38ab22a6f923d17d63874393
lcf <luocfprime@gmail.com>
2025-09-19 09:38:03 +0800

    feat: Add about file.

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__about__.py
@@ -0,0 +1,8 @@
+"""
+{{cookiecutter.package_name}} info file
+"""
+
+from importlib.metadata import version
+
+__application__ = "{{cookiecutter.package_name}}"
+__version__ = version(__application__)
---
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,10 @@
+"""
+{{cookiecutter.package_name}}
+"""
+
+from {{cookiecutter.package_name}}.__about__ import __application__, __version__
+
+__all__ = [
+    "__application__",
+    "__version__",
+]
